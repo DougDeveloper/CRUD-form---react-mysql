ce4733e4a6ab450ec0cd23e8bfd1a5369d01055f
DougDeveloper <d.ferreira.bat@gmail.com>
2023-01-16 18:14:36 -0300

    Inserindo e estilizando o formulário

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import GlobalStyle from "./styles/global"
+import styled from "styled-components"
+import Form from "./components/Form"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 800px;
+  margin-top: 10px;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2``
+
+function App() {
+  return (
+    <>
+      
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+      </Container>
+      
+        
+
+      <ToastContainer
+        autoClose={3000}
+        position={toast.POSITION.BOTTOM_LEFT}
+      />
+
+      <GlobalStyle />
+
+    </>
+  );
+}
+
+export default App;
